93af5ccc9628042d830b4d806bbc8f35f29b1255
IgnorantCoder <fr052123@gmail.com>
2017-08-18 15:21:37 +0900

    update

--- baby.hs
+++ baby.hs
@@ -143,3 +143,20 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
     where g x y = f y x
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n 
+    | even n = n : chain (n `div` 2)
+    | odd n  = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
